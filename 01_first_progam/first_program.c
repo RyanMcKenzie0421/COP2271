8043f0a3242c55ab6682a697150e105c606ad435
RyanMcKenzie0421 <rmckenzie2881@floridapoly.edu>
2026-05-20 18:15:06 +0000

    Create first program

---
+++ 01_first_progam/first_program.c
@@ -0,0 +1,24 @@
+// one line comment
+
+/*
+multiline comment
+*/
+
+//prepocessor directive (starts with #)
+//stdio is a standard input output. std standard i input o outpud
+// .h is a header file
+#include <stdio.h>
+
+//create the main function
+// return type name (parameters) {}
+
+int main(void) {       //void is optional here, it meants the main function will receive no information
+    printf("Welcome\nto\nCOP2271\n"); // \n - new line (escape character)
+    printf("Are\nyou\nready\nto\ncode?\n");
+    return 0;
+
+}
+
+
+
+//main function should be encolsed in brackets
